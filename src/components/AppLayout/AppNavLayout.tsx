c7fc1153d9548c4d742869996ea0ca07b90aac39
Gus <randomcash@tutanota.com>
2025-05-28 19:13:38 +0100

    fix: remove unused routes

--- src/components/AppLayout/AppNavLayout.tsx
+++ src/components/AppLayout/AppNavLayout.tsx
@@ -92,8 +92,6 @@ function AppNavLayout({
                 ? t('common.playground')
                 : pathname === '/staking'
                 ? t('staking.title')
-                : pathname === '/bridge'
-                ? t('bridge.title')
                 : ''}
             </Text>
           </HStack>
@@ -105,20 +103,6 @@ function AppNavLayout({
             <RouteLink href="/swap" isActive={pathname === '/swap'} title={t('swap.title')} />
             <RouteLink href="/liquidity-pools" isActive={pathname.includes('/liquidity')} title={t('liquidity.title')} />
             <RouteLink href="/portfolio" isActive={pathname === '/portfolio'} title={t('portfolio.title')} />
-            <RouteLink href="https://perps.raydium.io" isActive={false} title={t('perpetuals.title')} />
-            <RouteLink
-              href={`/launchpad${queryReferrer}`}
-              isActive={pathname.includes('/launchpad')}
-              title={
-                <Box as="span" bgGradient="linear-gradient(245.22deg, #FF2FC8 7.97%, #FFB12B 49.17%, #D3D839 92.1%)" bgClip="text">
-                  {t('launchpad.title')}
-                </Box>
-              }
-              slotAfter={<TagNewIcon />}
-              sx={{
-                gap: '0.25rem'
-              }}
-            />
             <Menu size="lg">
               <MenuButton fontSize={'lg'} px={4} py={2}>
                 <Flex
